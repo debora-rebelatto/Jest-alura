95c8137ba18da0b033df074e2d36aba29cbf7c98
debora-rebelatto <deborarebelatto1@gmail.com>
2023-01-12 22:53:15 -0300

    added tests

--- src/componentes/Formulario.test.tsx
+++ src/componentes/Formulario.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import Formulario from "./Formulario";
 
@@ -15,8 +15,24 @@ test("quando o input está vazio, novos participantes não podem ser adicionados
   // encontrar o botão
   const botao = screen.getByRole("button");
 
+  // inserir um valor no input
+  fireEvent.change(input, { target: { value: "João" } });
+
+  // garantir que o input está com o valor "João"
+  expect(input).toHaveValue("João");
+
+  // clica no botão
+  fireEvent.click(botao);
+
+  // garantir que o input esteja com o foco (está ativo)
+  // expect(input).toHaveFocus();
+
+  // garantir que o input esteja vazio
+  expect(input).toHaveValue("");
+
   // garantir que o input esteja no documento
   expect(input).toBeInTheDocument();
+
   // garantir que o botão esteja desabilitado
   expect(botao).toBeDisabled();
 });
