1cf8f7f738e7015fd5c208d03dae7ed8b5d368dd
debora-rebelatto <deborarebelatto1@gmail.com>
2023-01-12 22:43:22 -0300

    added prettier removed files

--- src/componentes/Formulario.test.tsx
+++ src/componentes/Formulario.test.tsx
@@ -4,19 +4,19 @@ import Formulario from "./Formulario";
 
 // Jest
 
-test('quando o input está vazio, novos participantes não podem ser adicionados', () => {
+test("quando o input está vazio, novos participantes não podem ser adicionados", () => {
+  render(<Formulario />);
 
-    render(<Formulario />)
+  // encontrar no DOM o input
+  const input = screen.getByPlaceholderText(
+    "Insira os nomes dos participantes"
+  );
 
-    // encontrar no DOM o input
-    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+  // encontrar o botão
+  const botao = screen.getByRole("button");
 
-    // encontrar o botão
-    const botao = screen.getByRole('button')
-
-    // garantir que o input esteja no documento
-    expect(input).toBeInTheDocument()
-    // garantir que o botão esteja desabilitado
-    expect(botao).toBeDisabled()
-
-})
+  // garantir que o input esteja no documento
+  expect(input).toBeInTheDocument();
+  // garantir que o botão esteja desabilitado
+  expect(botao).toBeDisabled();
+});
