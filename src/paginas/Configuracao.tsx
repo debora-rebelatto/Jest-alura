7ad88175990e976c07202a21157971e903cdcb99
debora-rebelatto <deborarebelatto1@gmail.com>
2023-01-31 15:22:07 -0300

    Folders

--- src/paginas/Configuracao.tsx
+++ src/paginas/Configuracao.tsx
@@ -1,19 +1,19 @@
-import Card from "../componentes/Card"
-import Formulario from "../componentes/Formulario"
-import ListaParticipantes from "../componentes/ListaParticipantes"
-import Rodape from "../componentes/Rodape"
+import Card from "../componentes/Card";
+import Formulario from "../componentes/Formulario/Formulario";
+import ListaParticipantes from "../componentes/ListaParticipantes/ListaParticipantes";
+import Rodape from "../componentes/Rodape/Rodape";
 
 const Configuracao = () => {
-    return (
-        <Card>
-            <section>
-                <h2>Vamos começar!</h2>
-                <Formulario />
-                <ListaParticipantes />
-                <Rodape />
-            </section>
-        </Card>
-    )
-}
+  return (
+    <Card>
+      <section>
+        <h2>Vamos começar!</h2>
+        <Formulario />
+        <ListaParticipantes />
+        <Rodape />
+      </section>
+    </Card>
+  );
+};
 
-export default Configuracao
+export default Configuracao;
